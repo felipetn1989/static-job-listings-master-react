4557da3f9a4fe74f10e86fce9441c38cf3fd17f9
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-06-09 18:37:04 -0300

    created Header.js

---
+++ listings/src/components/pages/Header.js
@@ -0,0 +1,9 @@
+import styles from './Header.module.css'
+
+export default function Header() {
+    return (
+        <header className={`${styles.header} h-20 w-full bg-[#5ba4a4]`}>
+
+        </header>
+    )
+}
